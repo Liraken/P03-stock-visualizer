649b949e20bdb9e287d2be6e81bafa6225887d16
AshelynAllred <AshelynAllred@gmail.com>
2024-02-04 15:06:01 -0600

    Added stock selection with search function
    - selection qty is limited to 5
    - added css entries to make selection formatting clearer

--- static/custom/js/plot.js
+++ static/custom/js/plot.js
@@ -1,7 +1,22 @@
-function plotStocks(traceArray, plotType) {
+function plotStocks(traceArray) {
+
+    console.log(traceArray);
+
+    let titleStr = "";
+    for(i = 0; i < traceArray.length; i++) {
+        if (i === 0) {
+            titleStr = `${traceArray[i].name}`;
+        } else {
+            titleStr = titleStr + ", " + `${traceArray[i].name}`;
+        }
+    };
+
+    console.log(titleStr);
 
     let layout = {
         dragmode: 'zoom',
+        template: 'plotly_dark',
+        title: titleStr
     };
 
     Plotly.newPlot("price-plot", traceArray, layout);
