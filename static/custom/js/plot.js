db876e8d86766989eab31714ce437f4a37f07b7a
AshelynAllred <AshelynAllred@gmail.com>
2024-02-04 23:29:36 -0600

    Added functions to handle trace list generation
    - refined plotting and how active tickers are determined
    - Added wordcloud library
    - changed organizedTickerData to use a static Object instead of parsing from CSV and reorganizing to JSON on load.

--- static/custom/js/plot.js
+++ static/custom/js/plot.js
@@ -1,23 +1,56 @@
 function plotStocks(traceArray) {
 
-    console.log(traceArray);
+    //console.log(traceArray);
 
-    let titleStr = "";
-    for(i = 0; i < traceArray.length; i++) {
-        if (i === 0) {
-            titleStr = `${traceArray[i].name}`;
-        } else {
+    let titleStr = '';
+    if (traceArray.length > 0) {
+        //preloading first ticker name into title string
+        titleStr = `Stock Prices in $USD: ${traceArray[0].name}`;
+        //iterating through rest of ticker names, adding them with comma separators
+        for(i = 1; i < traceArray.length; i++) {
             titleStr = titleStr + ", " + `${traceArray[i].name}`;
-        }
-    };
+        };
+    } else {
+        titleStr = 'Select one or more stock tickers(s) to see plot!'
+    }
 
-    console.log(titleStr);
+    //console.log(titleStr);
 
     let layout = {
         dragmode: 'zoom',
-        template: 'plotly_dark',
-        title: titleStr
+        //selectdirection: 'h',
+        title: titleStr,
+        width: plotDiv.clientWidth,
+        height: plotDiv.clientHeight,
+        margin: {
+            l: 30,
+            r: 30,
+            t: 90,
+            b: 30
+        },
+        plot_bgcolor: '#111',
+        paper_bgcolor: '#111',
+        font: {
+            color: '#eee'
+        },
+        line: {
+            color: '#eee'
+        },
+        xaxis: {
+            gridcolor: '#555'
+        },
+        yaxis: {
+            gridcolor: '#555'
+        }
     };
 
     Plotly.newPlot("price-plot", traceArray, layout);
+}
+
+function resizePlot() {
+    let update = {
+        width: plotDiv.clientWidth,
+        height: plotDiv.clientHeight
+    };
+    Plotly.relayout("price-plot", update);
 }
